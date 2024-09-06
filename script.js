96524a95317f773b8549dbeed7acf26799cc0230
Laurence Lesmoras <llesmoras@proton.me>
2024-09-06 21:56:04 +0800

    Added result output textbox with automated adjustment. Also removed someone we loved :(

--- script.js
+++ script.js
@@ -5,16 +5,34 @@ Developed by:
 - Laurence Lesmoras
 - Laurence Kharl Devera
 - Joshua Famor
-- Jasper Navarez
-
 */
+let currentMessage = "";
 
-
-// When the "Tokenize my text, please uwu!" is clicked, this will be executed.
+// When the "Tokenize my text, please uwu!" button is clicked, this will be executed.
 document.getElementById("submit").onclick = function() {
     let text = document.getElementById("userInput").value;
-    alert(text);
+    document.getElementById("totalCharacters").innerHTML = text.length;
+    addElementToTextBox(text);
 }
+
+// Function to append elements into the textbox
+function addElementToTextBox(message) {
+    const resultTextbox = document.getElementById("resultTextbox");
+    resultTextbox.value += message + "\n";
+    adjustHeight(resultTextbox);
+}
+
+
+function adjustHeight(textarea) {
+    textarea.style.height = "auto"; 
+    textarea.style.height = (textarea.scrollHeight) + "px"; 
+}
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    adjustHeight(document.getElementById("resultTextbox"));
+});
+
 
 // This is how a function works in JavaScript.
 function helloWorld() {
@@ -30,3 +48,4 @@ function helloWorldPeroMayParameters(name) {
 function add(a, b) {
     return a + b;
 }
+

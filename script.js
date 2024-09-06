a8f7d1747430d421de4064127b202dc2b6495dfe
testing1 <jFamor@mcm.edu.ph>
2024-09-06 20:54:25 +0800

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ Developed by:
 - Laurence Lesmoras
 - Laurence Kharl Devera
 - Joshua Famor
-- Jasper Navarez
 
 */
 
@@ -30,3 +29,45 @@ function helloWorldPeroMayParameters(name) {
 function add(a, b) {
     return a + b;
 }
+
+// Famor part
+// Yo this is para ma clasify ang mga token
+function classifyToken(token) {
+    if (/^[a-zA-Z]+$/.test(token)) {
+        return 'Word';
+    } else if (/^\d+$/.test(token)) {
+        return 'Number';
+    } else if (/^[a-zA-Z0-9]+$/.test(token)) {
+        return 'Alphanumeric';
+    } else if (/^[.,!?;:]+$/.test(token)) {
+        return 'Punctuation';
+    } else {
+        return 'Unknown';
+    }
+}
+
+// kani kay input
+function tokenize(input) {
+    // Step 1: Split the input string by the delimiter '<'
+    let tokens = input.split('<');
+    
+    // Step 2: Classify each token and store it in an array
+    let classifiedTokens = tokens.map(token => {
+        let type = classifyToken(token);
+        return { token, type };
+    });
+    
+    // Step 3: Further break down each token into individual characters
+    let granularTokens = classifiedTokens.map(entry => {
+        let chars = entry.token.split('');
+        return { ...entry, chars };
+    });
+    
+    return granularTokens;
+}
+
+
+    
+
+
+
